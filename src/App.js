9f9c6af1b8b66b4a0ddbc4e822b7337f142fbcca
Lalit Kumar <76099925+nanwallalit20@users.noreply.github.com>
2023-08-07 17:16:31 +0530

    firebase successfully added into cart component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,116 @@
 import React from "react";
 import Navbar from './Navabar'
 import Cart from './Cart'
+import { addDoc, collection, getDocs ,getFirestore, onSnapshot, doc, updateDoc, deleteDoc, query, where, orderBy} from 'firebase/firestore'; 
 
 
 class App extends React.Component {
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-        products:[{
-        id:1,
-        item:'Phone',
-        price :10000,
-        quantity: 1,
-        img:'https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/l/8/r/-original-imaghxemnnnkd8bg.jpeg?q=70'
-    
-        },
-        {
-        id:2,
-        item:'Earphone',
-        price :1000,
-        quantity: 1,
-        img:'https://rukminim2.flixcart.com/image/612/612/l0sgyvk0/headphone/e/w/c/buds-z2-oneplus-original-imagcg5gfpcg5ndv.jpeg?q=70'
-        },
-        {
-        id:3,
-        item:'Charger',
-        price :1200,
-        quantity: 1,
-        img:'https://rukminim2.flixcart.com/image/612/612/xif0q/battery-charger/x/i/y/original-20-watt-adapter-compatible-for-the-black-store-original-imagjnmpepnfryvh.jpeg?q=70'
-        }
-        
-    ]
+        products:[],
+        loading:true
 
     }
+   
+}
+componentDidMount(){
+ 
+  const firestore = getFirestore(this.props.firebaseApp);
+  const db = collection(firestore, 'Products')
+  //we will use getdocs for fetching document from db and use onsnapshot method rendering real time updates from db
+//query the data
+const q=query(db,where('price','==',99990 ),orderBy('price','asc'));
+
+  onSnapshot(q, (snapshot)=>{
+   const products= snapshot.docs.map((doc)=>{
+      const data=doc.data();
+      data['id']=doc.id;
+      return data;
+    })
+
+    this.setState({
+      products : products ,
+      loading:false
+    })
+      
+  })
+
 }
 
 handleIncreaseQuantity=(product)=>{
-        console.log('hey please update the product',product);
-
+      const firestore = getFirestore(this.props.firebaseApp);
+      const db = collection(firestore, 'Products');
         const{products}=this.state;
         let index=products.indexOf(product);
-        products[index].quantity+=1;
-        this.setState({
-            product:products,
+        // products[index].quantity+=1;
+        // this.setState({
+        //     product:products,
+        // })
+        const updatedQuantity = products[index].quantity + 1
+
+        const productDocRef = doc(db, products[index].id);
+        
+       
+        updateDoc(productDocRef,{
+          quantity:updatedQuantity
         })
+        .then(()=>{
+          console.log('quantity increases ')
+        })
+        .catch(err=>{
+          console.log('error in updating');
+        })
+       
+
+
 }
 handleDecreaseQuantity=(product)=>{
+  const firestore = getFirestore(this.props.firebaseApp);
+    const db = collection(firestore, 'Products');
     console.log('decrease the quantity');
     const{products}=this.state;
     let index=products.indexOf(product);
     if(products[index].quantity>0)
-    {products[index].quantity-=1;}
-    this.setState({
-        product:products,
-    })
+    {
+    const updatedQuantity = products[index].quantity - 1
+    const productDocRef = doc(db, products[index].id);
+        
+       
+        updateDoc(productDocRef,{
+          quantity:updatedQuantity
+        })
+        .then(()=>{
+          console.log('quantity decreases ')
+        })
+        .catch(err=>{
+          console.log('error in updating',err);
+        })
+      }   
+    // this.setState({
+    //     product:products,
+       
+    // })
 }
 handleDeleteProduct=( id)=>{
     console.log('delete the product',id);
     const{products}=this.state;
-   const items= products.filter((item)=>item.id !== id );
-   this.setState({
-    products :items,
-   })
+    const firestore = getFirestore(this.props.firebaseApp);
+    const db = collection(firestore, 'Products')
+  
+  //  const items= products.filter((item)=>item.id !== id );
+  //  this.setState({
+  //   products :items,
+  //  })
+
+  const product=doc(db,id);
+  deleteDoc(product)
+  .then(()=>{
+    console.log('delete product successfully!!')
+  })
+  .catch(err=>{
+    console.log('err',err);
+  })
 
 }
 getProductCount=()=>{
@@ -80,23 +129,42 @@ getProductCount=()=>{
   })
   return total;
  }
+ newProduct=()=>{
+  const firestore = getFirestore(this.props.firebaseApp);
+  const db = collection(firestore, 'Products');
+  const newItem={
+    item:'Speaker',
+    price:5000,
+    quantity:4,
+    imageURL:"https://images.unsplash.com/photo-161896"
+  }
+  addDoc(db,newItem)
+  .then((itemRef)=>{
+    console.log(itemRef);
+  })
+  .catch(err=>{
+    console.log('err',err);
+  })
+ }
  
 render(){
   
     const {handleDecreaseQuantity,handleDeleteProduct,handleIncreaseQuantity}=this
-    const{products}=this.state;
+    const{products,loading}=this.state;
   return (
     <div >
       
       <Navbar
         count={this.getProductCount()} 
       />
+      <button onClick={this.newProduct} style={{fontSize:20,backgroundColor:'lightgray',padding:10,cursor:"pointer"}}>Add product</button>
         <Cart
          products={products} 
          handleIncrease={handleIncreaseQuantity}
          handleDecrease={handleDecreaseQuantity}
          handleDelete={handleDeleteProduct}
         />
+        {loading && <h1>Loading.....</h1>}
 
         <div style={{fontSize:30,fontWeight:700,marginTop:20}}>
           Total Amount:{this.getTotalAmount()}/-
--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { initializeApp } from 'firebase/app';
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBEPjTp_KTJXLsOqfdzrFv2C35QMmZ5vI",
+  authDomain: "cart-product-add1a.firebaseapp.com",
+  projectId: "cart-product-add1a",
+  storageBucket: "cart-product-add1a.appspot.com",
+  messagingSenderId: "334980780413",
+  appId: "1:334980780413:web:cac03c7dcbef57800cbc14"
+};
+
+// Initialize Firebase 
+ const app=initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App firebaseApp={app} />
   </React.StrictMode>
 );
 
